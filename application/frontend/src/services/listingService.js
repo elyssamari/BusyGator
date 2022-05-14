d1b461f25dd39b002ae582fac264258306ee6d36
Vishal Sharma <vishalsharma1907@gmail.com>
2022-05-13 22:58:10 -0700

    Develop (#102)
    * Fixed issue preventing Signup.js "Confirm Password" validation from reflecting changes to "Password" (#95)
    
    * Move application text from footer to top navbar (M2NavBar.js) (#96)
    
    * Move application text from footer to top navbar (M2NavBar.js)
    
    * Add CSS to align Nav text
    
    * Removed negative margin for nav text in App.css
    
    * added google analytics (#98)
    
    * M4 post backend connection (#101)
    
    * Connected Post page to backend and added validation
    
    * Temporarily removed multer as a dependency
    
    * File upload and sql insert fixed
    
    * Fixed listing.controller to work with incrementing product_id and adjusted totalCount to more accurately reflect DB count
    
    * Updated npm build
    
    Co-authored-by: vishals9711 <vishalsharma1907@gmail.com>
    
    Co-authored-by: ajccarlson <32660587+ajccarlson@users.noreply.github.com>
    Co-authored-by: Sharaf <70246700+abdullahsharafsfsu@users.noreply.github.com>

--- application/frontend/src/services/listingService.js
+++ application/frontend/src/services/listingService.js
@@ -1,13 +1,14 @@
 /*
  * FILE: listingService.js
  *
- * AUTHOR(S): Siqi Guo, Vishal Ramanand Sharma, Samantha Saxton-Getty,
- * Elyssa Mari Tapawan
+ * AUTHOR(S): Aaron Carlson, Siqi Guo, Vishal Ramanand Sharma,
+ * Samantha Saxton-Getty, Elyssa Mari Tapawan
  *
  * PURPOSE: This file contains the services of the listing.
  */
 
 import axios from 'axios';
+var FormData = require('form-data');
 
 export const getAllListings = () => {
   return axios.get('/listing/getAllListings');
@@ -15,4 +16,22 @@ export const getAllListings = () => {
 
 export const getListingByFilter = (searchParams) => {
   return axios.get('/listing/getListingByFilter', { params: searchParams });
+};
+
+export const createListing = (listingInfo) => {
+  const formData = new FormData();
+  formData.append('imageFile', listingInfo.image);
+  formData.append('category', listingInfo.category);
+  formData.append('description', listingInfo.description);
+  formData.append('location', listingInfo.location);
+  formData.append('price', listingInfo.price);
+  formData.append('sellerID', listingInfo.sellerID);
+  formData.append('title', listingInfo.title);
+  console.log([...formData.values()]);
+  const config = {
+    headers: {
+      'content-type': 'multipart/form-data',
+    },
+  };
+  return axios.post('/listing/createListing', formData, config);
 };

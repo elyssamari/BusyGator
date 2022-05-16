3fc151ab236de1dd8a6cd405ff3b810fe4451610
ajccarlson <32660587+ajccarlson@users.noreply.github.com>
2022-05-16 14:24:35 -0700

    Edited getListing functions to only display posts that are approved (#104)

--- application/backend/controllers/listing.controller.js
+++ application/backend/controllers/listing.controller.js
@@ -13,7 +13,7 @@ var mime = require('mime-types');
 
 const getAllListings = async (req, res) => {
     try {
-        connection.query('SELECT * from product', (err, results) => {
+        connection.query('SELECT * from product where approved = 1', (err, results) => {
             let newResults = [];
             for (let index = 0; index < results.length; index++) {
                 const element = results[index];
@@ -38,11 +38,11 @@ const getListingByFilter = async (req, res) => {
         const { categoryId = "", searchText = "", min = "", max = "" } = req.query;
         let totalCount = 0;
         connection.query(`select count(*) as totalCount 
-        from product;`, (err, res) => {
+        from product where approved = 1;`, (err, res) => {
             totalCount = res[0].totalCount
         });
         if (categoryId !== "" && searchText === "") {
-            connection.query(`SELECT * from product where product.category=${categoryId}`, (err, results) => {
+            connection.query(`SELECT * from product where approved = 1 and product.category=${categoryId}`, (err, results) => {
                 let newResults = [];
                 for (let index = 0; index < results.length; index++) {
                     const element = results[index];
@@ -63,7 +63,7 @@ const getListingByFilter = async (req, res) => {
             });
         } else if (categoryId === "" && searchText !== "") {
             console.log(searchText)
-            connection.query(`SELECT * FROM product WHERE product.title LIKE "%${searchText}%" OR product.description LIKE "%${searchText}%";`, (err, results) => {
+            connection.query(`SELECT * FROM product WHERE approved = 1 and (product.title LIKE "%${searchText}%" OR product.description LIKE "%${searchText}%");`, (err, results) => {
                 let newResults = [];
                 for (let index = 0; index < results.length; index++) {
                     const element = results[index];
@@ -84,7 +84,7 @@ const getListingByFilter = async (req, res) => {
                 if (err) throw err
             });
         } else if (categoryId !== "" && searchText !== "") {
-            connection.query(`SELECT * FROM product WHERE product.category=${categoryId} AND (product.title LIKE "%${searchText}%" OR product.description LIKE "%${searchText}%");`, (err, results) => {
+            connection.query(`SELECT * FROM product WHERE approved = 1 and product.category=${categoryId} AND (product.title LIKE "%${searchText}%" OR product.description LIKE "%${searchText}%");`, (err, results) => {
                 let newResults = [];
                 for (let index = 0; index < results.length; index++) {
                     const element = results[index];
@@ -103,7 +103,7 @@ const getListingByFilter = async (req, res) => {
                 }); if (err) throw err
             });
         } else {
-            connection.query('SELECT * from product', (err, results) => {
+            connection.query('SELECT * from product where approved = 1', (err, results) => {
                 let newResults = [];
                 for (let index = 0; index < results.length; index++) {
                     const element = results[index];
@@ -147,7 +147,7 @@ const createListing = async (req, res) => {
         const { imageFile = "" } = req.files;
         let totalCount = 0;
         connection.query(`select count(*) as totalCount 
-        from product;`, (err, countResult) => {
+        from product where approved = 1;`, (err, countResult) => {
             totalCount = countResult[0].totalCount + 1;
             const fileName = title.trim().toLowerCase().replace(/ /g, "_") + "." + mime.extension(imageFile.mimetype);
             let baseSQL = "INSERT INTO product (seller_id, category, location, title, description, image, image_thumbnail, price, date_created) VALUES (?, ?, ?, ?, ?, ?, ?, ?, now());";
